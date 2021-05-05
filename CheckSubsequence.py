42692b9da8e67405229ceecaf348220b43de27a1
jainabhi2001 <55582546+jainabhi2001@users.noreply.github.com>
2021-05-06 01:38:52 +0530

    Update CheckSubsequence.py

--- CheckSubsequence.py
+++ CheckSubsequence.py
@@ -1,4 +1,8 @@
-
+""" This code takes 
+    i. first line as a string/combination of a virus named "coronavirus"
+    ii. second line as  "N" number of patients 
+    iii. rest "N" number of lines takes the patient's report content (which has some sequence of characters)
+    Note: We need to tell wether the patients's report sequence is a subsequence of "coronavirus".I f yes print "POSITIVE" else "NEGATIVE"
 
 def main():
  # Write code here
